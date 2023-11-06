3d2d50d99320cb847139d8c2db2ae92d86bf32c1
Rosen Todorov <rosen.vt55@gmail.com>
2023-11-06 15:33:05 +0200

    Added example form for comments in blogdetails

--- live-to-travel/src/components/BlogDetails.jsx
+++ live-to-travel/src/components/BlogDetails.jsx
@@ -67,6 +67,30 @@ export default function BlogDetails() {
             </div>
           </div>
         </div>
+        <div className="article-comments" >
+          <h4 className="text-uppercase mb-4" >Leave a comment</h4>
+          <form action="#" method="post" >
+            <div className="form-group">
+              <label htmlFor="name">Name *</label>
+              <input type="text" className="form-control" id="name"/>
+            </div>
+            <div className="form-group">
+              <label htmlFor="email">Email *</label>
+              <input type="email" className="form-control" id="email"/>
+            </div>
+            <div className="form-group">
+              <label htmlFor="website">Website</label>
+              <input type="url" className="form-control" id="website"/>
+            </div>
+            <div className="form-group">
+              <label htmlFor="message">Message *</label>
+              <textarea id="message" cols="30" rows="5" className="form-control"></textarea>
+            </div>
+            <div className="form-group mb-0">
+              <input type="submit" value="Leave a comment" className="btn-comment"/>
+            </div>
+          </form>
+        </div>
         </div>
 
         
