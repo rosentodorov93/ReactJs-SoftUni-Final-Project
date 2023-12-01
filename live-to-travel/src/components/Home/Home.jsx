ccb99a62edef9c59e236aa9a06ddc22ab7d81da7
Rosen Todorov <rosen.vt55@gmail.com>
2023-12-01 08:58:41 +0200

    Loaded latest three blogs in home page

--- live-to-travel/src/components/Home/Home.jsx
+++ live-to-travel/src/components/Home/Home.jsx
@@ -1,6 +1,19 @@
+import { useEffect, useState } from "react";
 import "./Home.css";
 
+import * as blogService from '../../services/blogService';
+import BlogItem from "../BlogItem/BlogItem";
+
 export default function Home() {
+
+  const [latestBlogs, setLatestBlogs] = useState([]);
+
+  useEffect(()=>{
+    blogService.getLatestsThree()
+    .then(res => setLatestBlogs(res)
+    .catch(err => console.log(err)))
+  },[])
+
   return (
     <>
       <section id="home" className="welcome-hero">
@@ -56,72 +69,8 @@ export default function Home() {
           </div>
           <div className="blog-content">
             <div className="row">
-              <div className="col-md-4 col-sm-6">
-                <div className="single-blog-item">
-                  <div className="single-blog-item-img">
-                    <img src="/images/blog/b1.jpg" alt="blog image" />
-                  </div>
-                  <div className="single-blog-item-txt">
-                    <h2>
-                      <a href="#">
-                        How to find your Desired Place more quickly
-                      </a>
-                    </h2>
-                    <h4>
-                      posted <span>by</span> <a href="#">admin</a> march 2018
-                    </h4>
-                    <p>
-                      Lorem ipsum dolor sit amet, consectetur de adipisicing
-                      elit, sed do eiusmod tempore incididunt ut labore et
-                      dolore magna.
-                    </p>
-                  </div>
-                </div>
-              </div>
-              <div className="col-md-4 col-sm-6">
-                <div className="single-blog-item">
-                  <div className="single-blog-item-img">
-                    <img src="/images/blog/b2.jpg" alt="blog image" />
-                  </div>
-                  <div className="single-blog-item-txt">
-                    <h2>
-                      <a href="#">
-                        How to find your Desired Place more quickly
-                      </a>
-                    </h2>
-                    <h4>
-                      posted <span>by</span> <a href="#">admin</a> march 2018
-                    </h4>
-                    <p>
-                      Lorem ipsum dolor sit amet, consectetur de adipisicing
-                      elit, sed do eiusmod tempore incididunt ut labore et
-                      dolore magna.
-                    </p>
-                  </div>
-                </div>
-              </div>
-              <div className="col-md-4 col-sm-6">
-                <div className="single-blog-item">
-                  <div className="single-blog-item-img">
-                    <img src="/images/blog/b3.jpg" alt="blog image" />
-                  </div>
-                  <div className="single-blog-item-txt">
-                    <h2>
-                      <a href="#">
-                        How to find your Desired Place more quickly
-                      </a>
-                    </h2>
-                    <h4>
-                      posted <span>by</span> <a href="#">admin</a> march 2018
-                    </h4>
-                    <p>
-                      Lorem ipsum dolor sit amet, consectetur de adipisicing
-                      elit, sed do eiusmod tempore incididunt ut labore et
-                      dolore magna.
-                    </p>
-                  </div>
-                </div>
-              </div>
+                {latestBlogs && latestBlogs.map(b => <BlogItem key={b._id} data={b}/>)}
+
             </div>
           </div>
         </div>
