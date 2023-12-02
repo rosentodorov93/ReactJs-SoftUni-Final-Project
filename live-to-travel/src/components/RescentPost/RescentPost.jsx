9f4c6b209e9a52b68b0b203580260156ed916e05
Rosen Todorov <rosen.vt55@gmail.com>
2023-12-02 12:30:59 +0200

    Added recentPost component

---
+++ live-to-travel/src/components/RescentPost/RescentPost.jsx
@@ -0,0 +1,28 @@
+import './RescentPost.css';
+import { useNavigate } from 'react-router-dom';
+
+export default function RescentPost({
+    _id,
+    imageUrl,
+    title,
+    _createdOn,
+}) {
+
+  const navigate = useNavigate();
+
+  return (
+    <div className="rescent-post" onClick={() => {navigate(`/blog/details/${_id}`)}}>
+      <div className='img-container'>
+      <img
+        className="post-img"
+        src={imageUrl}
+        alt={title}
+      />
+      </div>
+      <div className="pl-3">
+        <h6 className="m-1">{title}</h6>
+        <small>{new Date(_createdOn).toLocaleDateString()}</small>
+      </div>
+    </div>
+  );
+}
