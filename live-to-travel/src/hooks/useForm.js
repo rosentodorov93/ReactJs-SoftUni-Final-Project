c2af010f4acc6127205ebb266876222bcf48db10
Rosen Todorov <rosen.vt55@gmail.com>
2023-12-05 16:15:09 +0200

    added validator function in useForm

--- live-to-travel/src/hooks/useForm.js
+++ live-to-travel/src/hooks/useForm.js
@@ -1,8 +1,9 @@
 import { useState } from "react"
 
 
-export default function useForm(onSubmitHandler, initialValues){
+export default function useForm(onSubmitHandler, initialValues, validator){
     const [formValues, setFormValues] = useState(initialValues);
+    const [errors, setErrors] = useState({});
 
     const onChange = (e) => {
         setFormValues(state => ({
@@ -16,12 +17,21 @@ export default function useForm(onSubmitHandler, initialValues){
     const onSubmit = (e) =>{
         e.preventDefault();
 
+        if(validator){
+            const errorsResult = validator(formValues);
+            if(Object.values(errorsResult).some(x => x.length > 0)){
+                setErrors(errorsResult);
+                return;
+            }
+        }
+
         onSubmitHandler(formValues);
     }
 
     return {
         formValues,
         onChange,
-        onSubmit
+        onSubmit,
+        errors,
     }
 }
