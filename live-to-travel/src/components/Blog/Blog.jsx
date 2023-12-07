3db623c32210d92cdc70d9852666190a3e6d0c9f
Rosen Todorov <rosen.vt55@gmail.com>
2023-12-07 12:06:10 +0200

    Styled blog page

--- live-to-travel/src/components/Blog/Blog.jsx
+++ live-to-travel/src/components/Blog/Blog.jsx
@@ -11,43 +11,36 @@ import RescentPost from "../RescentPost/RescentPost";
 export default function Blog() {
   const [blogs, setBlogs] = useState([]);
   const [recentPosts, setRecentPosts] = useState([]);
-  const {category} = useParams();
+  const { category } = useParams();
 
-  const onSearchSubmit = async(values) => {
-    const result = await blogService.getAll(formValues.category, formValues.search);
+  const onSearchSubmit = async (values) => {
+    const result = await blogService.getAll(
+      formValues.category,
+      formValues.search
+    );
     setBlogs(result);
   };
   const { formValues, onChange, onSubmit } = useForm(onSearchSubmit, {
-    category: category ? category : '',
-    search: '',
+    category: category ? category : "",
+    search: "",
   });
 
   useEffect(() => {
     blogService
-      .getAll(category ? category : '')
+      .getAll(category ? category : "")
       .then((res) => setBlogs(res))
       .catch((err) => console.log(err));
-    
-    blogService.getLatestsThree()
-    .then(res => setRecentPosts(res))
-    .catch(err => console.log(err))
+
+    blogService
+      .getLatestsThree()
+      .then((res) => setRecentPosts(res))
+      .catch((err) => console.log(err));
   }, []);
 
   return (
     <>
-      <div className="container header">
-        <div className="container">
-          <div className="heading-wrapper">
-            <h3>Blog</h3>
-            <div>
-              <p>
-                <a href="https://www.free-css.com/free-css-templates">Home</a>
-              </p>
-              <i className="fa fa-angle-double-right "></i>
-              <p>Blog</p>
-            </div>
-          </div>
-        </div>
+      <div className="container blog-header">
+        <div className="container"></div>
       </div>
 
       {/* main part */}
@@ -56,44 +49,47 @@ export default function Blog() {
         <div className="container">
           <div className="search-container">
             <form className="search-form" method="GET" onSubmit={onSubmit}>
-            <div>
-              <label htmlFor="category">Category</label>
-            <select
-                className="search-type"
-                name="category"
-                id="category"
-                onChange={onChange}
-                value={formValues.category}
-                placeholder="category"
-              >
-                <option value={''} >All</option>
-                {categories.map((c) => (
-                  <option key={c} value={c}>{c}</option>
-                ))}
-              </select>
-            </div>
-              
-           <div>
-          <label htmlFor="search">Search</label>
-           <input
-              id="search"
-                type="text"
-                className="search-text"
-                name="search"
-                onChange={onChange}
-                value={formValues.search}
-                placeholder="Search Title..."
-              />
-              <button type="submit" className="btn-search">
-                Search
-              </button>
-           </div>
+              <div>
+                <label htmlFor="category">Category</label>
+                <select
+                  className="search-input"
+                  name="category"
+                  id="category"
+                  onChange={onChange}
+                  value={formValues.category}
+                  placeholder="category"
+                >
+                  <option value={""}>All</option>
+                  {categories.map((c) => (
+                    <option key={c} value={c}>
+                      {c}
+                    </option>
+                  ))}
+                </select>
+              </div>
 
+              <div className="search-wrapper">
+                <label htmlFor="search">Search</label>
+                <div className="search-box">
+                  <input
+                    id="search"
+                    type="text"
+                    name="search"
+                    onChange={onChange}
+                    value={formValues.search}
+                    placeholder="Search Title..."
+                  />
+                  <button type="submit" className="btn-search">
+                    Search
+                  </button>
+                </div>
+              </div>
             </form>
           </div>
 
           <div className="row">
             <div className="row-left-section">
+              <h4 className="blog-heading">Blog</h4>
               <div className="row">
                 {blogs.map((blog) => (
                   <BlogItem key={blog._id} data={blog} />
@@ -101,10 +97,11 @@ export default function Blog() {
               </div>
             </div>
             <div className="row-right-section">
-             
               <div className="mb-5">
                 <h4>Recent Post</h4>
-                {recentPosts.map(post => <RescentPost key={post._id} {...post}/>)}
+                {recentPosts.map((post) => (
+                  <RescentPost key={post._id} {...post} />
+                ))}
               </div>
             </div>
           </div>
