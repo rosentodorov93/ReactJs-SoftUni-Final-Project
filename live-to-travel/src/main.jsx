4563893f439805feae63de6208baadde869a7046
Rosen Todorov <rosen.vt55@gmail.com>
2023-12-07 11:07:47 +0200

    Added scrollToTop component

--- live-to-travel/src/main.jsx
+++ live-to-travel/src/main.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import {BrowserRouter} from 'react-router-dom';
 import App from './App.jsx'
+import ScrollToTop from './components/ScrollToTop/ScrollToTop.js';
 import './main.css'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
+  <ScrollToTop/>
     <App />
   </BrowserRouter>,
 )
