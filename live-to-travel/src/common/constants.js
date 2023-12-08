aef5890d8f6f5e733430b90636fea743f5eefd71
Rosen Todorov <rosen.vt55@gmail.com>
2023-12-08 19:27:13 +0200

    Added auth constants

--- live-to-travel/src/common/constants.js
+++ live-to-travel/src/common/constants.js
@@ -2,4 +2,6 @@ export const Constants = {
     defaultUserImage: 'https://www.shutterstock.com/image-vector/male-silhouette-avatar-profile-picture-260nw-199246382.jpg',
     textEditorApiKey: '96xztb739pac7nr7rdpsopcswma57vu98h3dk649gdul0ws8',
     fileStaxkApiKey: 'AZcmoUAkqSKecIlxiW0YDz',
+    persistedStateKey: 'auth',
+    token: 'accessToken'
 }
