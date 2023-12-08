8b28b39ed72d771bf89c3e0457cae0b281aeb03c
Rosen Todorov <rosen.vt55@gmail.com>
2023-12-08 13:22:13 +0200

    Added errorsMesages file

---
+++ live-to-travel/src/common/errorMesages.js
@@ -0,0 +1,24 @@
+export const PostErrorMesages = {
+    titleRequired: 'Title is required',
+    titileRange: 'Title must be between 2 and 100 charactesr long',
+    imageUrlRequired: 'Image is required',
+    validImageUrl: 'Please add valid image url',
+    contentRequired: 'Content is required',
+}
+
+
+export const UserErrorMesages = {
+    firstnameRequired: 'First name is required',
+    lastnameRequired: 'Last name is required',
+    firstnameRange: 'First name must be between 3 and 30 characters long',
+    lastnameRange: 'Last name must be between 3 and 30 characters long',
+    emailRequired: 'Email is required',
+    emailValid: 'Please add valid email address',
+    passwordRequired: 'Password is required',
+    confirmPasswordRequired: 'Confirm password is required',
+    passwordMisMatch: 'Passwords do no match',
+}
+
+export const CommentsErrorMesages = {
+    textRequired: 'Comments must have message',
+}
