c55b3d12c6b70215214c50d3e3d3f0452ef923ce
Rosen Todorov <rosen.vt55@gmail.com>
2023-12-08 19:28:25 +0200

    Added not found component

---
+++ live-to-travel/src/components/NotFound/NotFound.jsx
@@ -0,0 +1,15 @@
+import { Link } from "react-router-dom";
+import Path from "../../common/paths";
+
+import './NotFound.css'
+
+export default function NotFound(){
+
+    return(
+        <div className="not-found">
+        <h1>404 - Not Found</h1>
+        <p>Sorry, the page you are looking for does not exist.</p>
+        <Link to={Path.Home}>Home</Link>
+    </div>
+    )
+}
