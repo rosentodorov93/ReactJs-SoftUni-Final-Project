59bb44b5420f87ccf70420d5960aca7f6fc50267
Rosen Todorov <rosen.vt55@gmail.com>
2023-12-08 13:21:59 +0200

    Added constants file

---
+++ live-to-travel/src/common/constants.js
@@ -0,0 +1,5 @@
+export const Constants = {
+    defaultUserImage: 'https://www.shutterstock.com/image-vector/male-silhouette-avatar-profile-picture-260nw-199246382.jpg',
+    textEditorApiKey: '96xztb739pac7nr7rdpsopcswma57vu98h3dk649gdul0ws8',
+    fileStaxkApiKey: 'AZcmoUAkqSKecIlxiW0YDz',
+}
